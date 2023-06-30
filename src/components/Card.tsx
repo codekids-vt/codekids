3147b33c27caf0dce4f7b43d0f1d1ce0d0b3029f
golden2402 <golden2402@vt.edu>
2023-06-30 18:10:14 -0400

    add horizontal margins to card content

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,12 +1,12 @@
 function Card() {
     return (
-      <div className="max-w-sm bg-cardGreen rounded overflow-hidden shadow-lg">
+      <div className="max-w-sm m-2 drop-shadow-md">
         <img
-          className="w-full"
+          className="w-full object-cover rounded-lg"
           src="/images/card.png"
           alt="Sunset in the mountains"
-        ></img>
-        <div className="px-6 py-4">
+        />
+        <div className="px-6 py-4 mx-2 bg-cardGreen rounded-b">
           <p className="text-gray-700 text-base">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
             quia, nulla! Maiores et perferendis eaque, exercitationem praesentium
