f12375bea475117e0a5e1994b068eef3b2818cd9
antnguyen33 <ant33nguyen@gmail.com>
2023-07-08 20:50:13 -0400

    Added view all games button at bottom of site.

--- src/app/(base)/(pages)/home/page.tsx
+++ src/app/(base)/(pages)/home/page.tsx
@@ -1,7 +1,12 @@
+import Card from "@/components/Card";
+import ViewAllGamesButton from "@/components/ViewAllGamesButton";
+
 export default function Home() {
   return (
     <div>
       Home
+      <Card />
+      <ViewAllGamesButton />
     </div>
   );
 }
