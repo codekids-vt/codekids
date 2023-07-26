6882315a5cd1ca34c9aa4e30cdace5235cbf5640
golden2402 <golden2402@vt.edu>
2023-07-26 18:33:15 -0400

    draft structure for testing

--- src/app/(base)/(pages)/blockly-demo/page.tsx
+++ src/app/(base)/(pages)/blockly-demo/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import { Workspace } from "blockly";
 import { javascriptGenerator } from "blockly/javascript";
@@ -10,22 +10,47 @@ import toolbox from "./_toolbox";
 
 export default function BlocklyDemo() {
   const workspaceRef = useRef(null);
-  const [code, setCode] = useState<string>();
+
+  const [codeOutput, setCodeOutput] = useState<string>();
+  // if undefined, we had a successful test!:
+  const [codeResult, setCodeResult] = useState<Error>();
 
   useWorkspace({
     ref: workspaceRef,
     toolboxDefinition: toolbox,
     onWorkspaceChange: (workspace: Workspace) => {
-      console.log(javascriptGenerator.workspaceToCode(workspace));
+      setCodeOutput(javascriptGenerator.workspaceToCode(workspace));
     }
   });
+
+  const runTests = () => {
+    
+  }
   
   return (
-    <div className="mx-auto">
+    <div className="mx-auto my-8 max-w-4xl">
       <div 
         ref={workspaceRef}
-        className="mx-auto max-w-4xl h-[2000px]"
+        className="mx-auto mb-2 h-[720px]"
       />
+
+      <section className="mb-2">
+        <h1 className="font-bold text-xl">Testing</h1>
+        {
+          codeOutput
+            ? <pre className="font-mono">{codeOutput}</pre>
+            : <p>Run your code to get insight about bugs!</p>
+        }
+      </section>
+
+      <section>
+        <h1 className="font-bold text-xl">Your Code</h1>
+        {
+          codeOutput
+            ? <pre className="font-mono">{codeOutput}</pre>
+            : <p>Write some code and it&apos;ll pop up here!</p>
+        }
+      </section>
     </div>
   );
 }
