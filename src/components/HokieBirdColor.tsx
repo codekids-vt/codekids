799908506178c001eddd3126fae6de6670ec1c00
Joey El Shanti <jr.elshanti@gmail.com>
2023-09-25 21:13:12 -0400

    finished up framework gotta fix focusing on input.

--- src/components/HokieBirdColor.tsx
+++ src/components/HokieBirdColor.tsx
@@ -1,3 +1,4 @@
+import { setState } from "blockly/core/utils/aria";
 import React, { useState} from "react";
 
 interface HokieBirdColorState {
@@ -6,15 +7,51 @@ interface HokieBirdColorState {
     legs: string
 }
 
+const availableColors = ["red", "orange", "green", "maroon", "gold"]
+
 export const HokieBirdColoring = () => {
 
     const [colors, setColors] = useState({
         head: "transparent",
         body: "transparent",
         legs: "transparent"
-    })
+    });
+
+    const [textColors, setTextColors] = useState({
+        head: "",
+        body: "",
+        legs: "",
+    });
+
+
+    const [textHead, setTextHead] = useState("");
+    const [textBody, setTextBody] = useState("");
+    const [textLegs, setTextLegs] = useState("");
+    function handleOnSubmitColor (e: React.KeyboardEvent<HTMLInputElement>, part: string) {
+        if(e.key === 'Enter') {
+            const val = e.currentTarget.value.toLowerCase();
+            if (availableColors.includes(val)) {
+                const temp = colors[part as keyof HokieBirdColorState] =  val
+                const newColors = {
+                    ...colors,
+                    temp
+                }
+                setColors(newColors);
+            }
+        }
+    }
 
     
+    function handleOnColorChange (e: React.FormEvent<HTMLInputElement>, part: string) {
+        const temp = textColors[part as keyof HokieBirdColorState] =  e.currentTarget.value
+        const newColors = {
+            ...textColors,
+            temp
+        }
+        setTextColors(newColors);
+
+    }
+
     function handleOnDrop (e: React.DragEvent, part: string) {
         const color = e.dataTransfer.getData("Color") as string;
         const temp = colors[part as keyof HokieBirdColorState] =  color
@@ -40,19 +77,21 @@ export const HokieBirdColoring = () => {
             {/* <div>Drag The Colors over thse boxes</div> */}
             <div className="flex flex-col flex-grow justify-center items-center">
                 <div onDrop={(e) => handleOnDrop(e, "head")} onDragOver={(e) => handleDragOver(e)} className="flex outline-black outline-dotted">Hokie Head = 
-                    <input type="text" className="rounded"></input>
+                    <input key={1} value={textHead} type="text" className="rounded" placeholder="Drag or Type Here" onChange={e => setTextHead(e.target.value)} onKeyDown={(e) => handleOnSubmitColor(e, "head")}></input>
                 </div>
-                <div onDrop={(e) => handleOnDrop(e, "body")} onDragOver={(e) => handleDragOver(e)} className="flex outline-black mt-6 outline-dotted text-center">Hokie Body = 
-                    <input type="text" className="rounded"></input></div>
+                <div onDrop={(e) => handleOnDrop(e, "body")} onDragOver={(e) => handleDragOver(e)} className="flex outline-black mt-6 outline-dotted text-center" >Hokie Body = 
+                <input key={3} value={textBody} type="text" className="rounded" placeholder="Drag or Type Here" onChange={e => setTextBody(e.target.value)} onKeyDown={(e) => handleOnSubmitColor(e, "body")}></input>
+                </div>
                 <div onDrop={(e) => handleOnDrop(e, "legs")} onDragOver={(e) => handleDragOver(e)} className="flex outline-black mt-6 outline-dotted text-center">Hokie Legs = 
-                    <input type="text" className="rounded"></input></div>
+                <input key={2} value={textLegs} type="text" className="rounded" placeholder="Drag or Type Here" onChange={e =>setTextLegs(e.target.value) } onKeyDown={(e) => handleOnSubmitColor(e, "legs")}></input>
+                </div>
             </div>
             <div className="flex flex-row flex-grow justify-around">
-                <div draggable className="flex text-red-600" onDragStart={(e) => handleOnDrag(e, "Red")}>Red</div>
-                <div draggable className="flex text-orange-600" onDragStart={(e) => handleOnDrag(e, "Orange")}>Orange</div>
-                <div draggable className="flex text-green-700" onDragStart={(e) => handleOnDrag(e, "Green")}>Green</div>
-                <div draggable className="flex text-amber-950" onDragStart={(e) => handleOnDrag(e, "Maroon")}>Maroon</div>
-                <div draggable className="flex text-orange-400" onDragStart={(e) => handleOnDrag(e, "Gold")}>Gold</div>
+                <div draggable className="flex text-red-600" onDragStart={(e) => handleOnDrag(e, "red")}>Red</div>
+                <div draggable className="flex text-orange-600" onDragStart={(e) => handleOnDrag(e, "orange")}>Orange</div>
+                <div draggable className="flex text-green-700" onDragStart={(e) => handleOnDrag(e, "green")}>Green</div>
+                <div draggable className="flex text-amber-950" onDragStart={(e) => handleOnDrag(e, "maroon")}>Maroon</div>
+                <div draggable className="flex text-orange-400" onDragStart={(e) => handleOnDrag(e, "gold")}>Gold</div>
                 
             </div>
           </div>
