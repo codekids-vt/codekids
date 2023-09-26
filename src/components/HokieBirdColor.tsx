b99f0a09ac13a8b9fc0895ae72a98c027b898607
Joey El Shanti <jr.elshanti@gmail.com>
2023-09-26 18:09:04 -0400

    working with drag drop and fill in the blank

--- src/components/HokieBirdColor.tsx
+++ src/components/HokieBirdColor.tsx
@@ -10,88 +10,67 @@ interface HokieBirdColorState {
 const availableColors = ["red", "orange", "green", "maroon", "gold"]
 
 export function HokieBirdColoring() {
-
     const [colors, setColors] = useState({
-        head: "transparent",
-        body: "transparent",
-        legs: "transparent"
-    });
-
-    const [textColors, setTextColors] = useState({
         head: "",
         body: "",
-        legs: "",
+        legs: ""
     });
+   
+    function HokieBirdColors() {    
 
-
-    const [textHead, setTextHead] = useState("");
-
-    const [textBody, setTextBody] = useState("");
-    const [textLegs, setTextLegs] = useState("");
-    function handleOnSubmitColor (e: React.KeyboardEvent<HTMLInputElement>, part: string) {
-        if(e.key === 'Enter') {
-            const val = e.currentTarget.value.toLowerCase();
-            if (availableColors.includes(val)) {
-                const temp = colors[part as keyof HokieBirdColorState] =  val
-                const newColors = {
-                    ...colors,
-                    temp
+        function handleOnSubmitColor (e: React.KeyboardEvent<HTMLInputElement>, part: string) {
+            if(e.key === 'Enter') {
+                e.preventDefault();
+                const val = e.currentTarget.value.toLowerCase();
+                if (availableColors.includes(val)) {
+                    const temp = colors[part as keyof HokieBirdColorState] =  val
+                    const newColors = {
+                        ...colors,
+                        temp
+                    }
+                    setColors(newColors);
                 }
-                setColors(newColors);
             }
         }
-    }
-
     
-    function handleOnColorChange (e: React.FormEvent<HTMLInputElement>, part: string) {
-        const temp = textColors[part as keyof HokieBirdColorState] =  e.currentTarget.value
-        const newColors = {
-            ...textColors,
-            temp
+    
+        function handleOnDrop (e: React.DragEvent, part: string) {
+            const color = e.dataTransfer.getData("Color") as string;
+            const temp = colors[part as keyof HokieBirdColorState] =  color
+            const newColors = {
+                ...colors,
+                temp
+            }
+            setColors(newColors);
         }
-        setTextColors(newColors);
-
-    }
-
-    function handleOnDrop (e: React.DragEvent, part: string) {
-        const color = e.dataTransfer.getData("Color") as string;
-        const temp = colors[part as keyof HokieBirdColorState] =  color
-        const newColors = {
-            ...colors,
-            temp
+          
+        function handleDragOver(e: React.DragEvent) {
+            e.preventDefault()
         }
-        setColors(newColors);
-    }
-      
-    function handleDragOver(e: React.DragEvent) {
-        e.preventDefault()
-    }
-
-
-    function handleOnDrag(e: React.DragEvent, color: string) {
-        e.dataTransfer.setData("Color", color);
-    }
-
-    function HokieBirdColors() {      
+    
+    
+        function handleOnDrag(e: React.DragEvent, color: string) {
+            e.dataTransfer.setData("Color", color);
+        }
+        
         return (
             <div className="flex flex-col flex-grow mx-10">
-            {/* <div>Drag The Colors over thse boxes</div> */}
+            <div>Drag The Colors over thse boxes</div>
             <div className="flex flex-col flex-grow justify-center items-center">
                 <div onDrop={(e) => handleOnDrop(e, "head")} onDragOver={(e) => handleDragOver(e)} className="flex outline-black outline-dotted">Hokie Head = 
                     <input 
                     key={"headText"} type="text" 
                     className="rounded" 
                     placeholder="Drag or Type Here"  
-                    onChange={e => setTextHead(e.target.value)}
                     onKeyDown={(e) => handleOnSubmitColor(e, "head")}
-                    value={textHead}
+                    defaultValue={colors.head}
                     ></input>
                 </div>
                 <div onDrop={(e) => handleOnDrop(e, "body")} onDragOver={(e) => handleDragOver(e)} className="flex outline-black mt-6 outline-dotted text-center" >Hokie Body = 
-                <input key={"bodyTex"}  type="text" className="rounded" placeholder="Drag or Type Here" defaultValue={""} onKeyDown={(e) => handleOnSubmitColor(e, "body")}></input>
+                <input key={"bodyTex"}  type="text" className="rounded" placeholder="Drag or Type Here" defaultValue={colors.body} onKeyDown={(e) => handleOnSubmitColor(e, "body")}></input>
                 </div>
                 <div onDrop={(e) => handleOnDrop(e, "legs")} onDragOver={(e) => handleDragOver(e)} className="flex outline-black mt-6 outline-dotted text-center">Hokie Legs = 
-                <input key={"legsText"} type="text" className="rounded" placeholder="Drag or Type Here" defaultValue="" onKeyDown={(e) => handleOnSubmitColor(e, "legs")}></input>
+                <input key={"legsText"} type="text" className="rounded" placeholder="Drag or Type Here" defaultValue={colors.legs} onKeyDown={(e) => handleOnSubmitColor(e, "legs")}></input>
                 </div>
             </div>
             <div className="flex flex-row flex-grow justify-around">
