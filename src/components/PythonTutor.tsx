e4362beb7c4bafa93f23bb2abde668b78211cb88
noahpro <noahpro@gmail.com>
2023-11-08 14:21:41 -0500

    Add zoom functionality to PythonTutor component

--- src/components/PythonTutor.tsx
+++ src/components/PythonTutor.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { useState } from "react";
 
 import Editor from 'react-simple-code-editor';
@@ -18,17 +19,19 @@ export function PythonTutor({ props }: { props: any | IPythonTutorProps }) {
 
   const [editing, setEditing] = useState(false);
   const [code, setCode] = useState(props.code);
+  const [scale, setScale] = useState(1);
 
-  const width = props.width ?? 650;
-  const height = props.height ?? 400;
+  // calculate width and height based on device
   const cumulative = props.cumulative ?? false;
 
   return (
     <div className="flex flex-col flex-grow items-center w-full">
       {!editing &&
-        <iframe width={width} height={height} className="border-2 border-gray-300 shadow-2xl rounded-2xl"
-          src={`https://pythontutor.com/iframe-embed.html#code=${encodeURIComponent(code)}&cumulative=${cumulative}&py=2`}>
-        </iframe>
+        <div className="overflow-scroll flex-grow shadow-2xl rounded-2xl w-full h-full">
+          <iframe className="w-full h-full" style={{ scale: scale, transformOrigin: "top left" }}
+            src={`https://pythontutor.com/iframe-embed.html#code=${encodeURIComponent(code)}&cumulative=${cumulative}&py=2`}>
+          </iframe>
+        </div>
       }
       {editing &&
         <Editor
@@ -36,14 +39,22 @@ export function PythonTutor({ props }: { props: any | IPythonTutorProps }) {
           onValueChange={code => setCode(code)}
           highlight={code => highlight(code, languages.js)}
           padding={10}
-          style={{ fontFamily: '"Fira code", "Fira Mono", monospace', }}
+        style={{ fontFamily: '"Fira code", "Fira Mono", "monospace"', scale: scale, transformOrigin: "top left" }}
           className="w-10/12 text-md h-[calc(100vh-22rem)] overflow-y-scroll border-2 p-10 shadow-2xl rounded-xl"
         />
       }
-      <div className="p-4">
-        <button onClick={() => setEditing(!editing)} className="bg-primary-green hover:bg-hover-green hover:-translate-y-1 hover:shadow-2xl text-white font-bold py-2 px-4 rounded-full">
+      <div className="p-4 flex flex-row justify-center items-center space-x-4">
+        <button onClick={() => setEditing(!editing)} className="bg-primary-green text-white font-bold py-2 px-4 rounded-full">
           {editing ? "Run Code" : "Edit Code"}
         </button>
+        {!editing &&
+          <div className="flex flex-row justify-center items-center font-bold text-white">
+            <div className="bg-primary-green rounded-l-full py-2 px-4 hover:shadow-2xl ">Zoom</div>
+            <button onClick={() => setScale(scale * 1.2)} className=" p-2 border-x-2 border-white bg-primary-green">+</button>
+            <button onClick={() => setScale(scale / 1.2)} className=" p-2 rounded-r-full border-white bg-primary-green">-</button>
+          </div>
+        }
+
       </div>
     </div>
   );
