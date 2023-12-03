e0e8f6b23b6dcf46e3747184bcf59ddc0fcae022
Joey El Shanti <jr.elshanti@gmail.com>
2023-12-03 12:58:16 -0500

    changes discussed in last meeting

--- src/app/(base)/(pages)/books/[page]/page.tsx
+++ src/app/(base)/(pages)/books/[page]/page.tsx
@@ -10,7 +10,7 @@ function BookPreview({ BookData }: { BookData: Book }) {
         <div className="h-[325px] w-[275px] hover:shadow-2xl rounded-2xl hover:-translate-y-">
           <Image src={BookData.bookCover} width={275} height={280} alt={`Book Image-Background`} className="absolute" />
           <div className="relative top-12 left-4 w-52 h-64 flex-col flex items-center p-2 text-center">
-            <p className="mt-1 text-sm text-end text-black">{` Grades: ${BookData.gradeRange}`}</p>
+            <p className="mt-1 mb-2 text-sm text-end text-black">{` Grades: ${BookData.gradeRange}`}</p>
             <Image src={BookData.cover ? BookData.cover : BookData.pages[0].image} width={125} height={125} alt={`Book Image-${BookData.title}`} className="rounded-2xl" />
             <h1 className="text-black font-semibold">{BookData.title}</h1>
             <p className="mt-1 text-sm text-black">{BookData.blurb}</p>
