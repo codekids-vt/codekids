c9bfb720c8bb8cfb2aa00ba1fede1f5c046d4c78
scout517 <tdeverin17@gmail.com>
2024-02-10 18:17:35 -0500

    Split If-statement book into conditional/logical operators books

--- frontend/src/components/MisconceptionComponents/IfStatementIntro.tsx
+++ frontend/src/components/MisconceptionComponents/IfStatementIntro.tsx
@@ -3,30 +3,23 @@ import { Reader } from "../Reader";
 import { CodeSnippet } from "../CodeSnippet";
 import { Type, TypeStyle } from "../TypeStyle";
 
+
 export function IfStatementIntro() {
-  const p1Code = (
-    <p>
-      if it_is_raining:
-      <br />
-      {"\t"}print(
-      <TypeStyle text='"Stop Construction"' style={Type.STRING} />)<br />
-      else:
-      <br />
-      {"\t"}print(
-      <TypeStyle text='"Continue Construction"' style={Type.STRING} />)
-    </p>
-  );
+
+  const p1Code = <p>
+                      it_is_raining = <TypeStyle text='True' style={Type.BOOLEAN}/><br/>
+                      if it_is_raining:<br/>
+                      {"\t"}print(<TypeStyle text='"Stop Construction"' style={Type.STRING}/>)<br/>
+                      else:<br/>
+                      {"\t"}print(<TypeStyle text='"Continue Construction"' style={Type.STRING}/>)
+                  </p>
 
   return (
-    <div className="flex flex-col w-full items-center font-semibold text-lg text-center gap-3">
-      <Reader text="Example of if-statement" />
-      <img
-        width={400}
-        height={400}
-        src={"/IfStatementsBook/cloudy.jpg"}
-        alt="Image of construction"
-      />
-      <CodeSnippet code={p1Code} />
-    </div>
+      <div className='flex flex-col w-full text-center items-center font-semibold text-lg text-center gap-3'>
+          <img width={400} height={400} src={"/IfStatementsBook/cloudy.jpg"} alt='Image of construction'/>
+          <Reader text='Example of if-statement'/>
+          <CodeSnippet code={p1Code}/>
+      </div>
   );
 }
+
