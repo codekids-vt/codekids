9096d57984df90676e53d5197f05dab3356d2089
scout517 <tdeverin17@gmail.com>
2024-02-24 14:34:21 -0500

    All question redone. Python tutor can now take specific instructions. User can jump to correct line in python tutor

--- frontend/src/components/MisconceptionComponents/LifeOfMoose/MooseDr.tsx
+++ frontend/src/components/MisconceptionComponents/LifeOfMoose/MooseDr.tsx
@@ -29,6 +29,9 @@ export function MooseDr({
   const [q3Correct, setQ3Correct] = useState(false);
   const [q4Correct, setQ4Correct] = useState(false);
 
+  const [currentQuestion, setCurrentQuestion] = useState(0)
+  const [currentInstruction, setCurrentInstruction] = useState(0)
+
   React.useEffect(() => {
     if (props.pageNumber === 2) {
       setAllowNext(q1Correct && q2Correct && q3Correct && q4Correct);
@@ -41,6 +44,25 @@ export function MooseDr({
     props.pageNumber,
     setAllowNext,
   ]);
+
+  function getCurrentQuestion(questionNumber: number) {
+    setCurrentQuestion(questionNumber)
+  }
+
+  function getInstructionToJumpTo() {
+    switch (currentQuestion) {
+      case 0:
+        return 0;
+      case 1:
+        return 4;
+      case 2:
+        return 5;
+      case 3:
+        return 6;
+      default:
+       return 0;
+    }
+  }
 
   if (props.pageNumber === 1) {
     return getPage1();
@@ -66,36 +88,41 @@ export function MooseDr({
 
   function getPage2() {
     return (
-      <div className="flex flex-col w-full h-full text-center items-center gap-5">
-        <PythonTutor props={{ code: code }} />
-        <QuestionSet
-          correctAnswers={[q1Correct, q2Correct, q3Correct, q4Correct]}
-        >
-          <MultipleChoiceQuestion
-            question={q1.question}
-            answers={q1.answers}
-            style={Styles.HORIZONTAL}
-            setCorrect={setQ1Correct}
-          />
-          <MultipleChoiceQuestion
-            question={q2.question}
-            answers={q2.answers}
-            style={Styles.HORIZONTAL}
-            setCorrect={setQ2Correct}
-          />
-          <MultipleChoiceQuestion
-            question={q3.question}
-            answers={q3.answers}
-            style={Styles.HORIZONTAL}
-            setCorrect={setQ3Correct}
-          />
-          <MultipleChoiceQuestion
-            question={q4.question}
-            answers={q4.answers}
-            style={Styles.HORIZONTAL}
-            setCorrect={setQ4Correct}
-          />
-        </QuestionSet>
+      <div className="flex w-full h-full text-center items-start gap-5">
+        <div className="w-1/2 h-full"><PythonTutor props={{ code: code, instruction: currentInstruction }} /></div>
+        <div className="flex flex-col gap-5 w-1/2 items-center">
+          <QuestionSet
+            correctAnswers={[q1Correct, q2Correct, q3Correct, q4Correct]}
+            getCurrentQuestion={getCurrentQuestion}
+          >
+            <MultipleChoiceQuestion
+              question={q1.question}
+              answers={q1.answers}
+              style={Styles.HORIZONTAL}
+              setCorrect={setQ1Correct}
+            />
+            <MultipleChoiceQuestion
+              question={q2.question}
+              answers={q2.answers}
+              style={Styles.HORIZONTAL}
+              setCorrect={setQ2Correct}
+            />
+            <MultipleChoiceQuestion
+              question={q3.question}
+              answers={q3.answers}
+              style={Styles.HORIZONTAL}
+              setCorrect={setQ3Correct}
+            />
+            <MultipleChoiceQuestion
+              question={q4.question}
+              answers={q4.answers}
+              style={Styles.HORIZONTAL}
+              setCorrect={setQ4Correct}
+            />
+          </QuestionSet>
+          <Reader text="If you get lost, press this button to jump to the correct line in Python Tutor!"/>
+          <button className="border border-solid border-black w-fit py-3 px-5 rounded-3xl cursor-pointer bg-violet-300" onClick={() => setCurrentInstruction(getInstructionToJumpTo())}>{`Jump to line ${getInstructionToJumpTo() + 1}`}</button>
+        </div>
       </div>
     );
   }
--- frontend/src/components/QuestionSet.tsx
+++ frontend/src/components/QuestionSet.tsx
@@ -1,11 +1,14 @@
+import React from "react";
 import { useState } from "react";
 
 export function QuestionSet({
   children,
   correctAnswers,
+  getCurrentQuestion,
 }: {
   children: React.ReactElement[];
   correctAnswers: boolean[];
+  getCurrentQuestion?: (questionNumber: number) => void // Optional function to return the current question
 }) {
   const prevButtonStyle =
     "text-base bg-red-400 text-black border border-solid border-black rounded-3xl py-3.5 px-12 cursor-pointer whitespace-pre-wrap disabled:opacity-50";
@@ -14,6 +17,15 @@ export function QuestionSet({
   const border = "border-4 border-lime-300 p-4";
 
   const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [nextButtonText, setNextButtonText] = useState("Next Question →")
+
+  React.useEffect(() => {
+    if(currentQuestion === children.length - 1) {
+      setNextButtonText("Final Question")
+    } else {
+      setNextButtonText("Next Question →")
+    }
+  }, [currentQuestion, children.length])
 
   function canAdvance(): boolean {
     if (currentQuestion === children.length - 1) {
@@ -28,7 +40,7 @@ export function QuestionSet({
         Question {currentQuestion + 1}/{children.length}
       </p>
       {children.map((question, index) => (
-        <div hidden={currentQuestion !== index}>{question}</div>
+        <div hidden={currentQuestion !== index} key={index}>{question}</div>
       ))}
       <div className="flex flex-col-2 gap-96">
         <button
@@ -36,6 +48,7 @@ export function QuestionSet({
           disabled={currentQuestion === 0}
           onClick={() => {
             setCurrentQuestion(currentQuestion - 1);
+            if (getCurrentQuestion !== undefined) {getCurrentQuestion(currentQuestion - 1)}
           }}
         >
           {"← Previous Question"}
@@ -45,9 +58,10 @@ export function QuestionSet({
           disabled={!canAdvance()}
           onClick={() => {
             setCurrentQuestion(currentQuestion + 1);
+            if (getCurrentQuestion !== undefined) {getCurrentQuestion(currentQuestion + 1)}
           }}
         >
-          {"Next Question →"}
+          {nextButtonText}
         </button>
       </div>
     </div>
