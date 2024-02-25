539e929a74a54f51077c2e7551ac6b890993ee10
noahpro <noahpro@gmail.com>
2024-02-25 04:24:35 -0500

    add forgotten changes

---
+++ frontend/src/api/models/UserLightNoPassword.ts
@@ -0,0 +1,12 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+import type { AccountType } from "./AccountType";
+export type UserLightNoPassword = {
+  id: number;
+  token: string;
+  name?: string | null;
+  email?: string | null;
+  type: AccountType;
+};
---
+++ frontend/src/pages/AboutUsPage.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import Background from "../components/Background";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+export default function AboutUsPage() {
+  const studentVideos = [
+    {
+      title: "Puzzles to Pixels",
+      video: "https://www.youtube.com/embed/2w9z5Y8b4Y8",
+      image: "/anthony-video-preview.png",
+      author: "Anthony",
+      blurb:
+        "Learn about the world of Computer Science by watching this video!",
+      link: "/anthony-video.mp4",
+    },
+  ];
+  return (
+    <div>
+      <Background />
+      <Navbar />
+      <div className="flex flex-col gap-2 items-center container z-10 min-h-screen">
+        <div className="p-2 w-1/2">
+          <h1 className="text-4xl font-bold mt-10">About Us</h1>
+          <p className="text-lg mt-5">
+            Welcome to CodeKids! We are a team of students from Virginia Tech
+            working to provide free resources for kids to learn how to code. We
+            believe that coding is an essential skill for the future and we want
+            to make it accessible to everyone. Our goal is to provide a fun and
+            engaging environment for kids to learn how to code. We hope you
+            enjoy our resources and have fun learning how to code!
+          </p>
+          <h1 className="text-4xl font-bold mt-10">Featured Student Stories</h1>
+          {studentVideos.map((video, i) => (
+            <div key={i} className="flex flex-col gap-2">
+              <h2 className="text-2xl font-bold">{video.title}</h2>
+              <p className="text-lg">{video.blurb}</p>
+              <video src={video.link} controls className=""></video>
+            </div>
+          ))}
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
