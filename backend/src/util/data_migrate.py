1b149f7123d1eef7d019f6c543fb4c3b9d3b08ed
noahpro <noahpro@gmail.com>
2024-03-27 13:57:07 -0400

    migrate one book at a time

--- backend/src/util/data_migrate.py
+++ backend/src/util/data_migrate.py
@@ -1,52 +1,53 @@
 import json
 import asyncio
 from prisma import Prisma
+import argparse
 
-# Initialize Prisma client
 client = Prisma()
 
 
 async def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("id", type=int)
+    args = parser.parse_args()
+    id = args.id
+
     # Connect to the database
     await client.connect()
-
-    # Delete existing records from the Page table
-    await client.page.delete_many()
-
-    # Delete existing records from the Book table
-    await client.book.delete_many()
 
     # Open and load the JSON file
     with open("../frontend/src/util/books.json", "r") as file:
         books = json.load(file)
 
-    # Iterate over the books and insert them into the database
-    for book in books:
-        # Create book record
-        created_book = await client.book.create(
-            {
-                "title": book["title"],
-                "bookCover": book["bookCover"],
-                "blurb": book["blurb"],
-                "coverImage": book["cover"] if book.get("cover") else None,
-                "author": book["author"],
-                "gradeRange": book["gradeRange"],
-                "category": book["category"],
-                "pages": {
-                    "create": [  # type: ignore
-                        {
-                            "content": json.dumps(page["content"]),
-                            "image": page["image"],
-                            "props": json.dumps(page.get("props", {})),
-                            "pageNumber": i + 1,
-                        }
-                        for i, page in enumerate(book["pages"])
-                    ]
-                },
-            }
-        )
+    book = [book for book in books if book["BookId"] == id][0]
+    if not book:
+        print(f"Book with id {id} not found")
+        return
+    # Create book record
+    created_book = await client.book.create(
+        {
+            "title": book["title"],
+            "bookCover": book["bookCover"],
+            "blurb": book["blurb"],
+            "coverImage": book["cover"] if book.get("cover") else None,
+            "author": book["author"],
+            "gradeRange": book["gradeRange"],
+            "category": book["category"],
+            "pages": {
+                "create": [  # type: ignore
+                    {
+                        "content": json.dumps(page["content"]),
+                        "image": page["image"],
+                        "props": json.dumps(page.get("props", {})),
+                        "pageNumber": i + 1,
+                    }
+                    for i, page in enumerate(book["pages"])
+                ]
+            },
+        }
+    )
 
-        print(f"Inserted book: {created_book.title}")
+    print(f"Inserted book: {created_book.title}")
 
     # Disconnect the database
     await client.disconnect()
