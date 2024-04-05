5e848f5b76eb8823b2df3f8a5d96614d1e56bb8e
noahpro <noahpro@gmail.com>
2024-04-05 00:42:07 -0400

    about us page fixes

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -10,10 +10,10 @@ export default function Footer() {
   return (
     <footer className="flex tablet:flex-col justify-center bottom-0 p-4 bg-white shadow-black shadow z-[100]">
       <span className="flex-1 p-2 tablet:text-center">
-        &copy; KiData Project 2023. All Rights Reserved.
+        &copy; Codekids 2023. All Rights Reserved.
       </span>
       <ul className="p-auto my-auto tablet:text-center text-sm">
-        <FooterLink name="About" href="/about" />
+        <FooterLink name="About" href="/about_us" />
         <FooterLink name="Contact" href="/contact" />
         <FooterLink name="License" href="/license" />
       </ul>
