66d836ff0e164f3f9cc81a8330f9783d174fa1e3
noahpro <noahpro@gmail.com>
2024-04-08 12:02:11 -0400

    image question fix

--- frontend/src/components/ImageQuestion.tsx
+++ frontend/src/components/ImageQuestion.tsx
@@ -11,14 +11,16 @@ export function ImageQuestion({
   const [correct, setCorrect] = useState(false);
 
   useEffect(() => {
-    if (correct) {
-      setAllowNext(true);
-    }
-  }, [correct]);
+    setAllowNext(correct);
+  }, [correct, setAllowNext]);
 
   return (
     <div className="w-full h-full flex flex-col items-center justify-center">
-      <img src={props.image} className="h-1/2 aspect-square" />
+      <img
+        src={props.image}
+        alt={props.question}
+        className="h-1/2 aspect-square"
+      />
       <MultipleChoiceQuestion
         question={props.question}
         answers={props.answers}
