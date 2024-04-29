0b765c106d828bd8eb9a7561531dcffef5d1be82
gholder513 <gholder513@gmail.com>
2024-04-29 12:06:05 -0400

    Complete Activity To Move on Changes - Food Truck Activity

--- frontend/src/components/FoodTruckActivity.tsx
+++ frontend/src/components/FoodTruckActivity.tsx
@@ -41,10 +41,11 @@ export function FoodTruckActivity({
   };
 
   React.useEffect(() => {
+    setAllowNext(false);
     const all = selectedOptions.length === options.length;
     if (all) {
       const timeSpent = Math.round((new Date().getTime() - startTime) / 1000);
-      setAllowNext(all);
+      setAllowNext(true);
       playCorrectSound();
       handleInteraction("completed", true, timeSpent, user?.id);
     }
