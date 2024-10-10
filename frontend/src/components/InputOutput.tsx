2bd95f3f5d2e800f5862dbf4392e9b693950978a
laithmasri <laith.masri@icloud.com>
2024-10-10 18:48:49 -0400

    #380 testing branch and creating activity file.

---
+++ frontend/src/components/InputOutput.tsx
@@ -0,0 +1,4 @@
+import React, { useState } from "react";
+import useSound from "use-sound";
+import { useAuth } from "../context/AuthContext";
+import { handleInteraction } from "../util/interaction";
