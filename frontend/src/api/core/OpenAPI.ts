55981a1b48ce97118890ffa464e4ab03810a0808
sahana-alt <sahana.bhaskar4@gmail.com>
2025-04-21 17:49:51 -0400

    OpenAPIUpdated

--- frontend/src/api/core/OpenAPI.ts
+++ frontend/src/api/core/OpenAPI.ts
@@ -21,8 +21,7 @@ export type OpenAPIConfig = {
 
 export const OpenAPI: OpenAPIConfig = {
   BASE:
-    //"https://codekids-backend.endeavour.cs.vt.edu",
-    "http://localhost:8080",
+    "https://codekids-backend.endeavour.cs.vt.edu",
   VERSION: "0.1.0",
   WITH_CREDENTIALS: false,
   CREDENTIALS: "include",
